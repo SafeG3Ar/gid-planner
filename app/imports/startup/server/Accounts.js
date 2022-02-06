3193f0c110edce574c6e01303925fb1653fec160
Glen Larita <glarita@hawaii.edu>
2022-02-06 09:37:06 -1000

    Update Accounts.js

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -24,7 +24,15 @@ function createUser(email, password, role, firstName, lastName, phone, userName)
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, role, firstName, lastName, phone, userName }) => createUser(email, password, role, firstName, lastName, phone, userName));
+    Meteor.settings.defaultAccounts.map(({ email, password, role, firstName, lastName, phone, userName }) => createUser(
+      email,
+      password,
+      role,
+      firstName,
+      lastName,
+      phone,
+      userName,
+    ));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
