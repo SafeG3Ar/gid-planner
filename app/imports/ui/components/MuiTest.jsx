c1af69cb742700bf52d2f5db3907de7738d1b599
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2022-02-08 23:11:22 -0800

    added the navbar

--- app/imports/ui/components/MuiTest.jsx
+++ app/imports/ui/components/MuiTest.jsx
@@ -4,16 +4,31 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter } from 'react-router-dom';
 // import { withRouter, NavLink } from 'react-router-dom';
-import { Box, Typography } from '@material-ui/core';
+import { AppBar, Box, Toolbar, Typography } from '@material-ui/core';
 // import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
     // const menuStyle = { marginBottom: '10px' };
+    // this is creating a theme
+    // const theme = createTheme({
+    //   palette: {
+    //     primary: '#651fff',
+    //     secondary: '#4dabf5',
+    //   },
+    // });
     return (
-      <Box sx={{ width: '100%', maxWidth: 500 }}>
-        <Typography variant="h2" component="div" > This is the app. </Typography>
+      <Box sx={{ width: '100%' }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h2" component="div" > This is the app. </Typography>
+            {/* <IconButton size="large"
+              aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton> */}
+          </Toolbar>
+        </AppBar>
       </Box>
     );
   }
