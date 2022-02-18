55bf7b6ff9b3c0c8fa87dd7dff9d56a183423567
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2022-02-17 19:14:58 -0800

    color

--- app/imports/ui/components/MuiTest.jsx
+++ app/imports/ui/components/MuiTest.jsx
@@ -1,48 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import { withRouter } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+// import { Meteor } from 'meteor/meteor';
+// import { withTracker } from 'meteor/react-meteor-data';
+// import { withRouter } from 'react-router-dom';
 // import { withRouter, NavLink } from 'react-router-dom';
-import { AppBar, Box, Toolbar, Typography } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
+// import Person from '@mui/icons-material/Person';
+import { Person } from '@material-ui/icons';
 // import { Roles } from 'meteor/alanning:roles';
 
+const useStyles = makeStyles({
+  button: {
+    color: 'white',
+    backgroundColor: 'black',
+  },
+});
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
-class NavBar extends React.Component {
-  render() {
-    // const menuStyle = { marginBottom: '10px' };
-    // this is creating a theme
-    // const theme = createTheme({
-    //   palette: {
-    //     primary: '#651fff',
-    //     secondary: '#4dabf5',
-    //   },
-    // });
-    return (
-      <Box sx={{ width: '100%' }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h2" component="div" > This is the app. </Typography>
-            {/* <IconButton size="large"
-              aria-label="menu" sx={{ mr: 2 }}>
-              <MenuIcon />
-            </IconButton> */}
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
-  }
-}
-
-// Declare the types of all properties.
-NavBar.propTypes = {
-  currentUser: PropTypes.string,
+const MuiTest = () => {
+  const classes = useStyles();
+  return (
+    <Button
+      className={classes.button}
+      variant='contained'
+      size='large'
+      startIcon={<Person />}
+    >
+        Material UI
+    </Button>
+  );
 };
-
-// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-const NavBarContainer = withTracker(() => ({
-  currentUser: Meteor.user() ? Meteor.user().username : '',
-}))(NavBar);
-
-// Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter
-export default withRouter(NavBarContainer);
+export default MuiTest;
