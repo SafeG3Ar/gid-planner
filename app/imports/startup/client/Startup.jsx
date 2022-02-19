c414a037086a92354feaf4eaa11cd85091d4b7f7
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2022-02-18 19:21:53 -0800

    removed theme stuff

--- app/imports/startup/client/Startup.jsx
+++ app/imports/startup/client/Startup.jsx
@@ -1,18 +1,14 @@
-import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App.jsx';
-import { theme } from './theme';
 
 /* global document */
 
 // Startup the application by rendering the App layout component.
 Meteor.startup(() => {
   render(
-    <ThemeProvider theme= {theme} >
-      <App />
-    </ThemeProvider>,
+    <App />,
     document.getElementById('root'),
   );
 });
