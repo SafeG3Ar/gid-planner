c73fa3c12295db8fa6eb935312c8faed3b9b6754
Glen Larita <glarita@hawaii.edu>
2022-02-20 23:39:06 -1000

    testing mui packages

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
+import { Button, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const handleClick = () => {
+  console.log('testing click button');
+};
+
+const handleClick1 = () => {
+  console.log('testing icon button click');
+};
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -14,6 +24,12 @@ class Landing extends React.Component {
         <Grid.Column width={8}>
           <h1>Welcome to this template</h1>
           <p>Now get to work and modify this app!</p>
+          <Button onClick={handleClick} variant='contained' style={{ backgroundColor: 'purple' }}>
+            Testing Click
+          </Button>
+          <IconButton onClick={handleClick1}>
+            <DeleteIcon/>
+          </IconButton>
         </Grid.Column>
 
       </Grid>
