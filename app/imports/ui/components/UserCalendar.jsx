57ef606442036b781b84c14b759313ee38ae2625
alyssandra-cabading <alyssand@hawaii.edu>
2022-02-21 16:18:58 -1000

    Update UserCalendar.jsx

--- app/imports/ui/components/UserCalendar.jsx
+++ app/imports/ui/components/UserCalendar.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import CalendarPicker from '@mui/lab/CalendarPicker';
-// import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import { brown } from '@mui/material/colors';
+import Divider from '@mui/material/Divider';
+import { grey } from '@mui/material/colors';
+import moment from 'moment';
 
 const UserCalendar = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
-  const primary = brown[50];
-
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const primary = grey[100];
+  // {const showDate = moment(selectedDate).format('MM/DD/YYYY');}
   return (
     <Box
       sx={{
@@ -25,13 +27,15 @@ const UserCalendar = () => {
       }}
       justifyContent="center"
     >
-      <Paper elevation={8}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Paper elevation={8} >
+        <LocalizationProvider dateAdapter={AdapterDateFns} >
           <CalendarPicker
             date={selectedDate}
-            onChange={date => setSelectedDate(date)
-            } />
+            onChange={date => setSelectedDate(date)}/>
         </LocalizationProvider>
+        <Divider />
+        <Typography variant="h4" align="center">{moment(selectedDate).format('MMMM DD, YYYY')}</Typography>
+        <Divider />
         { /* Current Date or Select Date Shown Here */ }
         { /* Here the scheduled to do tasks will be shown in accordance to the selected date */ }
       </Paper>
