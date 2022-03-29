a225212c8af9b768fc1f3a0262dc12cd5fbbc10e
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2022-03-29 00:18:40 -0700

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Button, Container, Grid, Header } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+
+const logo = '../images/landing/gidcheck-transparent.png';
+const padding = { paddingLeft: '95px' };
 
 /** A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
     <Container text textAlign='center'>
       <Grid.Column width={9}>
-        <Header as='h1'>Welcome to <p>GID PLANNER</p></Header>
+        <Header as='h1'>Welcome</Header>
+        <Image src={logo} style={padding}/>
         <Header as='h3'><i>Have a task in mind? Get it Done with GID!</i></Header>
         <Header as='h2'>Let us GID started</Header>
         <Container textAlign='center'>
