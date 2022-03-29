7420b0d5960d45ccabd4456e1dab673e980aebad
alyssandra-cabading <alyssand@hawaii.edu>
2022-03-28 17:29:57 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Button, Container, Grid, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
-class Landing extends React.Component {
-  render() {
-    return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
-    );
-  }
-}
+const Landing = () => (
+  <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+    <Container text textAlign='center'>
+      <Grid.Column width={9}>
+        <Header as='h1'>Welcome to <p>GID PLANNER</p></Header>
+        <Header as='h3'><i>Have a task in mind?</i></Header>
+        <Header as='h2'>Get it Done with GID!</Header>
+        <Container textAlign='center'>
+          <Button size="medium" as={Link} to='/Signup'>
+            <i>GID Started</i>
+          </Button>
+        </Container>
+      </Grid.Column>
+    </Container>
+  </Grid>
+);
 
 export default Landing;
