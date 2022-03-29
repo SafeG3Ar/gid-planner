eb3207ca32ef8440e7a27392534cb076ed205c6d
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2022-03-28 23:33:25 -0700

    stuff

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Segment, Image } from 'semantic-ui-react';
+
+const logo = '../landing/images/gidcheck-transparent.png';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <Segment>
+        <Grid columns={3} relaxed='very'>
+          <Grid.Column>
+            <Image source={logo} size='small' />
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+        </Grid>
+      </Segment>
     );
   }
 }
